bb1c174b08c16501f679a52821503615233abb35
Pankaj Roy <127432477+pankajroy97@users.noreply.github.com>
2023-08-21 19:19:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,47 @@
-import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
+import pandas as pd 
+import numpy as np
+import pickle
+from sklearn import *
+import streamlit as st 
+import warnings
+warnings.filterwarnings('ignore')
 
-df = pd.read_csv("CAR DETAILS (1).csv")
+# Load the data and model
+df = pickle.load(open('dataframe.pkl', 'rb'))
+model=pickle.load(open('random_forest.pkl','rb'))
 
-x = df.drop('selling_price', axis=1)
-y = df['selling_price']
+def predict_sales( Country, Region, Segment, Category, SubCategory, Discount_Percentage, Actual_Discount, Quantity, Order_month):
+    # Create a new dataframe with the user inputs
+    inputs = pd.DataFrame([[Country, Region, Segment, Category, SubCategory, Discount_Percentage, Actual_Discount, Quantity, Order_month]],
+                          columns=['Country', 'Region', 'Segment', 'Category', 'Sub-Category','Discount_Percentage', 'Actual_Discount', 'Quantity', 'Order_month'])
+    
+    
+    # Check that the input data has the expected number of features
+    if inputs.shape[1] != 9:
+        raise ValueError(f'Expected 9 features but got {inputs.shape[1]}')
 
-categorical_columns = ['name', 'fuel', 'seller_type', 'transmission', 'owner']
+    # Make the prediction
+    sales = model.predict(inputs)
 
-# Apply one-hot encoding to the categorical columns
-preprocessor = ColumnTransformer(
-transformers=[('encoder', OneHotEncoder(), categorical_columns)],
-remainder='passthrough'
-)
-
-x_encoded = preprocessor.fit_transform(x)
-
-from sklearn.model_selection import train_test_split
-
-# Split the dataset into training and testing sets
-x_train, x_test, y_train, y_test = train_test_split(x_encoded, y, test_size=0.2, random_state=42)
-
-# Initialize the Random Forest Regressor
-rf_regressor = RandomForestRegressor()
-
-# Train the model
-rf_regressor.fit(x_train, y_train)
+    return sales[0]
 
 
-import streamlit as st
-# Create the Streamlit app
-st.title('Car Selling Price Prediction')
+# Create the user interface
+st.title('Sales prediction specific category')
+st.markdown('Use this app to predict sales for a specific category based on various input parameters.')
+Country = st.selectbox('Country', df['Country'].unique())
+Region = st.selectbox('Region', df['Region'].unique())
+Segment = st.selectbox('Segment', df['Segment'].unique())
+Category = st.selectbox('Category', df['Category'].unique())
+subcategories = df.loc[df['Category'] == Category, 'Sub-Category'].unique()
+SubCategory = st.selectbox('Sub-Category', subcategories)
+Discount_Percentage = float(st.selectbox('Discount Percentage', df['Discount_Percentage'].unique()))
+Actual_Discount = st.number_input('Actual Discount', min_value=0)
+Quantity = st.number_input('Quantity', min_value=1,max_value=9)
+Order_month = st.selectbox('Order month', df['Order_month'].unique())
 
-# User input for feature values
-user_input = {}
-for column in x.columns:
-    if column in categorical_columns:
-        unique_values = df[column].unique()
-        user_input[column] = st.selectbox(column, unique_values)
-    else:
-        user_input[column] = st.number_input(column, value=0)
 
-# Transform user input to one-hot encoding
-user_input_encoded = preprocessor.transform(pd.DataFrame(user_input, index=[0]))
-
-# Make predictions using the trained model
-prediction = rf_regressor.predict(user_input_encoded)
-
-# Display the prediction
-st.subheader('Prediction')
-st.write(f'The predicted selling price is: {prediction[0]}')
+# Add a button to trigger the prediction
+if st.button('Predict Price'):
+    Sales = predict_sales(Country, Region, Segment, Category, SubCategory, Discount_Percentage, Actual_Discount, Quantity, Order_month)
+    st.success(f'The predicted sales for {Category} in {Country} is {Sales:,.0f} Indian Rupees.')
